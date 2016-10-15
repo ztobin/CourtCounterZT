1673e5d21b6ae9bf9ef3cc0218c2903d6027bed0
TobinH4HQ <ztobin@pasedfoundation.org>
2016-10-15 14:13:34 -0700

    Milestone 3.5

--- app/src/main/java/com/tobincorporated/courtcounterzt/MainActivity.java
+++ app/src/main/java/com/tobincorporated/courtcounterzt/MainActivity.java
@@ -1,5 +1,6 @@
 package com.tobincorporated.courtcounterzt;
 
+import android.media.MediaPlayer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -8,18 +9,25 @@ import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
     private TextView infinityScoreView;
+    private int infinityScore;
+    private MediaPlayer bikeHorn;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
         infinityScoreView = (TextView)findViewById(R.id.infinityScoreView);
-
+        infinityScore =0;
         infinityScoreView.setText("0");
+        bikeHorn = MediaPlayer.create(this , R.raw.bikehorn);
+
     }
 
     public void infinityClick(View view){
-
-        infinityScoreView.setText("1");
+        infinityScore++;
+        bikeHorn = MediaPlayer.create(this , R.raw.bikehorn);
+        bikeHorn.start();
+        infinityScoreView.setText("" + infinityScore);
     }
 }
