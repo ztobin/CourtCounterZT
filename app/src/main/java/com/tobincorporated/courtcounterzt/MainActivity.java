2eccdf5aa6938317227fd06719c8057171433ba3
TobinH4HQ <ztobin@pasedfoundation.org>
2016-10-15 13:45:07 -0700

    Milestone 2

--- app/src/main/java/com/tobincorporated/courtcounterzt/MainActivity.java
+++ app/src/main/java/com/tobincorporated/courtcounterzt/MainActivity.java
@@ -2,12 +2,24 @@ package com.tobincorporated.courtcounterzt;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-
+    private TextView infinityScoreView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        infinityScoreView = (TextView)findViewById(R.id.infinityScoreView);
+
+        infinityScoreView.setText("0");
+    }
+
+    public void infinityClick(View view){
+
+        infinityScoreView.setText("1");
     }
 }
